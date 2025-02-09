2949f7adcb87e2250fc9baedce23a3573b9104f0
Kiamova Elvira <ela.kiyamova@yandex.ru>
2025-02-09 10:29:14 +0500

    feat: implement a drop-down list of news

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 const closeMenuElement = document.querySelector('.header__close-burger');
 const navigationMenuElement = document.querySelector('.header__nav');
 const burgerMenuElement = document.querySelector('.header__burger-menu');
+const dropDownElement = document.querySelector('.header__drop-down');
+const selectNewsElement = document.querySelector('.news-select');
+const newsListElements = document.querySelectorAll('[data-category]');
+
+
+const onOpenNewsList = () => {
+  dropDownElement.classList.add('header__drop-down_opened');
+}
+selectNewsElement.addEventListener('mouseenter', onOpenNewsList);
+
+newsListElements.forEach(item => {
+    item.addEventListener('click', () => {
+        if (item.style.backgroundColor === 'rgb(253, 199, 13)') {
+            item.style.backgroundColor = '';
+        } else {
+            item.style.backgroundColor = 'rgb(253, 199, 13)';
+        }
+    });
+});
+
+document.addEventListener("click", function(event) {
+  if (!dropDownElement.contains(event.target)) {
+    dropDownElement.classList.remove('header__drop-down_opened');
+    newsListElements.forEach(item => {
+        item.style.backgroundColor = '';
+    });
+  }
+});
+
 
 const onOpenBurgerMenu = (event) => {
   event.stopPropagation();
